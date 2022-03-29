a349d746e5755d47ecfcc04a945a65d176056403
victoraav <victoraavianna@gmail.com>
2022-03-29 16:01:38 -0300

    Alterações e adições ao layout

--- password-gen.py
+++ password-gen.py
@@ -1,10 +1,13 @@
 import random
+from turtle import width
+from numpy import pad
 import requests
 from tkinter import *
 from tkinter import ttk
 
 root = Tk()
-root.title("Password Generator - @victoraav")
+root.title("@victoraav - PassGen")
+root.iconbitmap("lock.ico")
 
 frm = ttk.Frame(root,padding=40)
 frm.grid()
@@ -14,6 +17,12 @@ ttk.Label(frm,text="PASSWORD GENERATOR",font=("Impact",20)).grid(column=0,row=0)
 
 ttk.Checkbutton(frm, text=" Capital Letter").grid(column=0, row=1, sticky=SW)
 ttk.Checkbutton(frm, text=" Special characters").grid(column=0, row=2, sticky=SW)
+
+ttk.Button(frm, text= "Generate").grid(column=0, row=3, pady=20)
+
+ttk.Entry(frm, width=30).grid(column=0, row=4, pady=5)
+ttk.Button(frm, text="Show", width=5).grid(column=0, row=5)
+ttk.Button(frm, text="Copy", width=5).grid(column=0, row=6)
 
 
 
