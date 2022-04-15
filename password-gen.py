3ee7a3b181767eea5504371f2b27fc7fd880364f
Victor Vianna <victoraavianna@gmail.com>
2022-04-15 15:35:36 -0300

    version 1.0 password generator

--- password-gen.py
+++ password-gen.py
@@ -1,9 +1,40 @@
-import random
-import requests
 from tkinter import *
 from tkinter import ttk
+import pyperclip
+import random
+import requests
 
 root = Tk()
+
+senhag = StringVar()
+senhag2 = ""
+
+def gerar():
+    words_site = "https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt"
+
+    response = requests.get(words_site)
+    words = response.content.splitlines()
+    numrandom = random.randint(0,40000)
+    palavra_aleatoria = words[numrandom].decode("utf-8")
+    simbolos = ["!","@","#","$","%","&","*"]
+
+    numero_aleatorio = random.randint(0,9999)
+    numero_gerado = str(numero_aleatorio)
+
+    numero_simbolo = random.randint(0,6)
+    simbolo_aleatorio = simbolos[numero_simbolo]
+
+    senha = palavra_aleatoria.capitalize() + numero_gerado + simbolo_aleatorio
+    global senhag
+    global senhag2
+    senhag.set(senha)
+    senhag2 = senha
+    return (senha)
+
+def copiar():
+    global senhag2
+    pyperclip.copy(senhag2)
+
 root.title("@victoraav - PassGen")
 root.iconbitmap("lock.ico")
 
@@ -12,15 +43,11 @@ frm.grid()
 
 ttk.Label(frm,text="PASSWORD GENERATOR",font=("Impact",20)).grid(column=0,row=0)
 
+ttk.Button(frm, text= "Generate", command= gerar).grid(column=0, row=3, pady=20)
 
-ttk.Checkbutton(frm, text=" Capital Letter").grid(column=0, row=1, sticky=SW)
-ttk.Checkbutton(frm, text=" Special characters").grid(column=0, row=2, sticky=SW)
+ttk.Entry(frm,textvariable=senhag ,width=30).grid(column=0, row=4, pady=5)
 
-ttk.Button(frm, text= "Generate").grid(column=0, row=3, pady=20)
-
-ttk.Entry(frm, width=30).grid(column=0, row=4, pady=5)
-ttk.Button(frm, text="Show", width=5).grid(column=0, row=5)
-ttk.Button(frm, text="Copy", width=5).grid(column=0, row=6)
+ttk.Button(frm, text="Copy", width=5,command= copiar).grid(column=0, row=5)
 
 
 
